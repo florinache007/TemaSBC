9d28d7bc70424553c79a7c4c9840a4aaf60b0279
florinsimion <florinsimion@Florin.domain.bisnet.ro>
2018-01-12 11:53:25 +0200

    dasdasdsadasd

---
+++ src/main/java/com/tema/model/Player.java
@@ -0,0 +1,44 @@
+package com.tema.model;
+
+public class Player {
+	
+	private String name;
+	private String team;
+	private int age;
+	private String gender;
+	private int goldenBall;
+	
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getTeam() {
+		return team;
+	}
+	public void setTeam(String team) {
+		this.team = team;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getGoldenBall() {
+		return goldenBall;
+	}
+	public void setGoldenBall(int goldenBall) {
+		this.goldenBall = goldenBall;
+	}
+
+}
---
+++ src/main/java/com/tema/model/Rule.java
@@ -0,0 +1,70 @@
+package com.tema.model;
+
+public class Rule {
+	
+	private String what1;
+	private String what2;
+	private String how1;
+	private String how2;
+	private String than1;
+	private String than2;
+	private String op;
+	private String is;
+	private int rule_nr;
+	
+	public int getRule_nr() {
+		return rule_nr;
+	}
+	public void setRule_nr(int rule_nr) {
+		this.rule_nr = rule_nr;
+	}
+	public String getWhat1() {
+		return what1;
+	}
+	public void setWhat1(String what1) {
+		this.what1 = what1;
+	}
+	public String getWhat2() {
+		return what2;
+	}
+	public void setWhat2(String what2) {
+		this.what2 = what2;
+	}
+	public String getHow1() {
+		return how1;
+	}
+	public void setHow1(String how1) {
+		this.how1 = how1;
+	}
+	public String getHow2() {
+		return how2;
+	}
+	public void setHow2(String how2) {
+		this.how2 = how2;
+	}
+	public String getThan1() {
+		return than1;
+	}
+	public void setThan1(String than1) {
+		this.than1 = than1;
+	}
+	public String getThan2() {
+		return than2;
+	}
+	public void setThan2(String than2) {
+		this.than2 = than2;
+	}
+	public String getOp() {
+		return op;
+	}
+	public void setOp(String op) {
+		this.op = op;
+	}
+	public String getIs() {
+		return is;
+	}
+	public void setIs(String is) {
+		this.is = is;
+	}
+
+}
